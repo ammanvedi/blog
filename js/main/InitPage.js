675d81cb1f62bbff35f3ebc9284067b33f794e08
amman vedi <amman.vedi@pulselive.com>
2016-02-28 22:47:08 +0000

    update todo

--- js/main/InitPage.js
+++ js/main/InitPage.js
@@ -1,9 +1,11 @@
 /**
  * todo - get a blog id from the url + update the currently reading post and update the url
  * todo - provide a home link
- * todo - decide wather to display atricle on load and load one more at a time, or load say 5 and show headers with options to read for each, then retrieve article text on request
- * todo - create new small loader icon
- * todo - auto load new article
+ * todo - decide weather to display article on load and load one more at a time, or load say 5 and show headers with options to read for each, then retrieve article text on request
+ * todo - create new small loader icon / partial
+ * todo - write readme
+ * todo - compress images in build
+ * todo - auto load new article with scroll loader
  * todo - decide weather a post list could work and if so how ( needs to be minimal  )
  */
 
